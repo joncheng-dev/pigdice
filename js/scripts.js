182dd2ed085466ab559aa51666d87afb61dac44e
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-23 15:38:45 -0700

    pass test 8
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,31 +12,61 @@ Player.prototype.updateScore = function (roundScore) {
   this.score += roundScore;
 }
 
-function turn() {
-  let temp = 0;
-  let rollAgain = true;
+//Player.prototype.switchPlayers = function () {
+  //needs start and end
+  //for start, assign to Homer
+  //for end, it will mean that you hit the button or get roll a 1
+//}
 
-  while (rollAgain === true) {
+// function switchPlayers() {
+  
+// }
+
+function rollLogic(roundPoints) {
+  // while (rollAgain === true) {
     let result = diceRoll();
+
     if (result === 1) {
-      temp = 0;
-      console.log(temp);
-      rollAgain = false;
+      roundPoints = 0;
+      // switchPlayers();
     } else {
-      temp = temp + result;
-      console.log(temp);
+      roundPoints = roundPoints + result;
+      console.log(roundPoints);
+    // }
     }
-  }
+  return roundPoints;
 }
- 
-
-
-
-
 
 // User Interface Logic
+window.addEventListener("load", gameSetter);
+
+function displayTotalScore (p1, p2){
+  document.getElementById("player-1-score").innerText = p1.score;
+  document.getElementById("player-2-score").innerText = p2.score;
+}
 
 
+function playerTurn(playerNumber) {
+  let roundPoints = 0;
+  let currentPoints = rollLogic(roundPoints);
+  // something here for allowing you roll again.
+  // update the score once you're done with your turn.
+  playerNumber.updateScore();
+  if (playerNumber.score >= 100) {
+    // playerNumber is the winner
+  } else {
+    // switch players function
+  }
+}
 
+function gameSetter() { 
+  let playerOne = new Player("Homer");
+  let playerTwo = new Player("Barney");
 
+  displayTotalScore(playerOne, playerTwo);
 
+  // Determine whose turn it is, so that when "roll again" button is clicked, it affects that player's roundPoints score.
+
+  document.getElementById("call-turn-function").addEventListener("click", p1Turn);
+
+}
