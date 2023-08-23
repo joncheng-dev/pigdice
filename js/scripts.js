9f116cc8f15800ffc9f261122c3f5986d0457277
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-23 10:58:32 -0700

    pass test 3
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,40 +8,17 @@ function Player(name) {
   this.score = 0;
 }  
 
-
+function turn() {
+  let temp = 0;
+  let result = diceRoll();
+  temp = temp + result;
+  console.log(temp);
+}
 
 
 // User Interface Logic
 
-// A function that rolls a die.
-
-// Initializes a temp score
-// let temp = 0;
-
-// If roll is 1, temp score is set to 0.
-// And turn is over.
-
-// If roll is 2-6, add to the temp score.
-// (player can click roll to keep rolling)
-// (player can click end turn to add temp to player score)
-
-// Change to next player.
-
-// click -> person clicks button to roll
-// click -> person clicks end turn to save score to object
 
 
 
-function turn() {
-  let temp = 0;
-  
-  
-  let result = diceRoll();
-  if (result === 1) {
-    //end turn and flip to next player
-  } else {
-    temp = temp += result;
-    //player decides to keep going UI
-    //player clicks a button, which triggers diceRoll
-  }
-}
+
